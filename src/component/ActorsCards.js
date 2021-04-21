c3d9134da468bb6817146e20d8c9b54be9dedb5e
Zehavit <levi.zehavit@gmail.com>
2021-04-21 10:47:35 +0300

    add fix to tapping last name before first name

--- src/component/ActorsCards.js
+++ src/component/ActorsCards.js
@@ -4,7 +4,7 @@ export default function ActorsCards(props) {
     let actorsCards;
     let actors = props.actors;
 
-    actors = actors.filter((actor) => (actor["firstName"].toLowerCase() + " " + actor["lastName"].toLowerCase()).includes(props.filterBy));
+    actors = actors.filter((actor) => (actor["firstName"].toLowerCase() + " " + actor["lastName"].toLowerCase()).includes(props.filterBy) || (actor["lastName"].toLowerCase() + " " + actor["firstName"].toLowerCase()).includes(props.filterBy));
     
     actors = actors.sort((a, b) => a[props.sortBy] > b[props.sortBy] ?1 : -1);
     actorsCards = actors.map((actor, index) => <ActorCard key={index} actor={actor} />)
